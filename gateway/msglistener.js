edf48eab8f8cdd78a46f023b83bf59da2b77efbd
yschen5812 <herbert5812@gmail.com>
2016-03-25 22:57:48 -0400

    Added files via upload
    mv msglistener.js

--- gateway/msglistener.js
+++ gateway/msglistener.js
@@ -32,20 +32,40 @@ MsgListener.prototype.startServer = function () {
     if (request.url.startsWith('/put'))//(request.method === "PUT")
     {
       logger.debug(`Got request from url=${request.url}`);
-      var body = [];
-      request
-        .on('data', function(chunk) {
-          // aggregate data into 'body' before receiving 'end' signal
-          body.push(chunk);
-        })
-        .on('end', function() {
-          //body = JSON.parse(Buffer.concat(body).toString());
-          logger.debug(`Received message body=${JSON.stringify(body, null ,2)}`);
+      if (request.url.indexOf('&') !== -1)
+      {
+        // request from browser, parse the command
+        var fn = request.url.substring(1, request.url.indexOf('&'));
+        var body = request.url.substring(request.url.indexOf('&') + 1);
+        body = queryStirng.parse(body);
+        this.dispatchEvent(fn, body);
+        response.write("<html>");
+        response.write("<head>");
+        response.write("<meta HTTP-EQUIV='Content-Type' CONTENT='text/html; charset=UTF-8'");
+        response.write("</head>");
+        response.write("<body>");
+        response.write("<button>一條</button> <button>一條</button> <button>三條</button> <button>四條</button> <button>五條</button> <button>六筒</button> <button>七筒</button> <button>八筒</button> <button>九萬</button> <button>九萬</button> <button>九萬</button> <button>北風</button> <button>北風</button> <button>北風</button>");
+        response.write("</body>");
+        response.write("</html>");
+        response.end();
+      }
+      else
+      {
+        var body = [];
+        request
+          .on('data', function(chunk) {
+            // aggregate data into 'body' before receiving 'end' signal
+            body.push(chunk);
+          })
+          .on('end', function() {
+            body = JSON.parse(Buffer.concat(body).toString());
+            logger.debug(`Received message body=${JSON.stringify(body, null ,2)}`);
 
-          var fn = request.url.substr(1);
-          this.dispatchEvent(fn, body);
-          response.end();
-        }.bind(this))
+            var fn = request.url.substr(1);
+            this.dispatchEvent(fn, body);
+            response.end();
+          }.bind(this))
+      }
     }
     else
     {
@@ -63,7 +83,7 @@ MsgListener.prototype.dispatchEvent = function (fn, data) {
   }
   else
   {
-    logger.log(`${fn} is not registered with this listener.`);
+    logger.error(`${fn} is not registered with this listener.`);
   }
 };
 
@@ -74,7 +94,7 @@ MsgListener.prototype.registerEvent = function(event) {
   }
   else
   {
-    logger.log("event '", event, "' is registered.");
+    logger.error(`event ${event} is registered.`);
   }
 }
 
