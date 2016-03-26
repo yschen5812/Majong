99f0572e6761a1bca732fbc942831123cc982304
yschen5812 <herbert5812@gmail.com>
2016-03-25 22:59:25 -0400

    Added files via upload
    add global.js to control global variables

---
+++ global.js
@@ -0,0 +1,10 @@
+module.exports = global;
+
+var global = {
+  serverIP        :  "127.0.0.1",
+  serverPort      :  8080,
+  clientIP        :  "127.0.0.1",
+  clientPort      :  80,
+  maxBoardPlayers :  4,
+  maxBoardViewers :  1000
+};
